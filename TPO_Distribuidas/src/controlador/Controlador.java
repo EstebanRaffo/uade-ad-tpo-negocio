f21b604669e94c9d3893461fd8246435e322cbcb
MANU PONSA <manuponsa@gmail.com>
2019-06-05 18:51:17 -0300

    "Arreglo en el update del m

--- TPO_Distribuidas/src/controlador/Controlador.java
+++ TPO_Distribuidas/src/controlador/Controlador.java
@@ -88,6 +88,7 @@ public class Controlador {
 	/*Probado*/
 	public void modificaProducto(ProductoView recibido) throws ProductoException{
 		Producto producto = ProductoDAO.getInstancia().findProductoByIdentificador(recibido.getIdentificador());
+		producto.setPrecio(recibido.getPrecio());
 		producto.update();
 	}
 	/*Probado*/
--- TPO_Distribuidas/src/negocio/Producto.java
+++ TPO_Distribuidas/src/negocio/Producto.java
@@ -74,4 +74,5 @@ public class Producto {
 			this.precio = precio;
 	}
 	
+	
 }
